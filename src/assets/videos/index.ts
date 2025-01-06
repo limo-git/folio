0dc15eb65bd5923c42827cbcc1310ac0791de3df
limo-git <Dimpleyadav32@gmail.com>
2025-01-06 20:41:57 +0530

    vid issue

---
+++ src/assets/videos/index.ts
@@ -0,0 +1,8 @@
+import earthLottie from './earth.json';
+import contact from './contact.mp4';
+import work5 from './work5.mp4';
+import work6 from './work6.mp4';
+import work7 from './work7.mp4';
+import work8 from './work8.mp4';
+
+export { earthLottie, contact, work5,work6, work7,work8 };
